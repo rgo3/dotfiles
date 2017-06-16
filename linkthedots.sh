50269d40779f59a7a5ed74b5231f8b174feb3152
Robin Gögge <r.goegge@outlook.com>
2017-06-16 18:58:43 +0200

    linkthedots.sh -> script to make symlinks on my dotfiles

---
+++ linkthedots.sh
@@ -0,0 +1,7 @@
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/config/i3/config ~/.config/i3/config
+ln -s ~/.dotfiles/config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+ln -s ~/.dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
+
