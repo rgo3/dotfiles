a954b87401010f0f80923dded84731534161afa9
Robin Gögge <r.goegge@outlook.com>
2017-06-20 23:15:59 +0200

    Revert "moved .tmux to dotfiles"
    This reverts commit c69d5cad47663441f40e81a1113c38dd4fffa47e.

--- linkthedots.sh
+++ linkthedots.sh
@@ -4,5 +4,4 @@ ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/config/i3/config ~/.config/i3/config
 ln -s ~/.dotfiles/config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
 ln -s ~/.dotfiles/config/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/.dotfiles/config/tmux/ ~/.tmux
 
